c53062aa085f1aae31747eb2ae9f4210e006a1f4
Naredla Anurag Reddy <n.anurag@beta-cae.in>
2022-12-19 16:41:57 +0530

    latest_code

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Anurag Reddy Naredla",
+    author_email="anuragreddy1241@gmail.com",
+    packages=find_packages()
+)
